6c9adfc3f6ef9bec5f53f90bb2db7f83a2d630ba
Tashi Dendup <tashi.dendup@dcpl.bt>
2020-08-28 17:32:21 +0600

    Parent to module parent in rails application

--- app/mailers/concerns/display_name_assigner.rb
+++ app/mailers/concerns/display_name_assigner.rb
@@ -5,7 +5,7 @@ module DisplayNameAssigner
 
   def default_sender_address
     address = Mail::Address.new("no-reply@#{URI.parse(ENV['HOST_URL']).host}")
-    address.display_name = ENV.fetch('MAIL_DISPLAY_NAME', Rails.application.class.parent.to_s)
+    address.display_name = ENV.fetch('MAIL_DISPLAY_NAME', Rails.application.class.module_parent.to_s)
     address
   end
 end
