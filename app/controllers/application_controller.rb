bd5431305afdd2eed96632cbc955e8d9b84a10f1
Tashi Dendup <tashi.dendup@dcpl.bt>
2021-06-01 11:40:59 +0600

    Define root directory in ruby 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -15,4 +15,8 @@ class ApplicationController < ActionController::API
 
   protect_from_forgery with: :exception
   skip_before_action :verify_authenticity_token
+
+  def after_sign_in_path_for(resource_or_scope)
+    stored_location_for(resource_or_scope) || root_path
+  end
 end
