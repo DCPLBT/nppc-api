8ca40d5075f41932e18245467f0721c432c5e6cd
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-01-11 17:21:09 +0600

    Cart features

---
+++ app/forms/cart_form.rb
@@ -0,0 +1,23 @@
+class CartForm < BaseForm
+  attr_accessor :cart
+
+  def index
+    update_cart
+    cart
+  end
+
+  private
+
+  def update_cart
+    return if line_items.empty?
+
+    cart.update(
+      quantity: line_items.sum(&:quantity),
+      amount: line_items.sum(&:total_price)
+    )
+  end
+
+  def line_items
+    @line_items ||= cart.line_items
+  end
+end
