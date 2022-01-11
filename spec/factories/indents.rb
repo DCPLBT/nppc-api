05ebe27289af778200139982445815f8a07a8138
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-01-11 23:23:58 +0600

    Indent update

--- spec/factories/indents.rb
+++ spec/factories/indents.rb
@@ -13,21 +13,21 @@
 #  district_id  :bigint
 #  extension_id :bigint
 #  region_id    :bigint
-#  stock_id     :bigint
+#  user_id      :bigint           not null
 #
 # Indexes
 #
 #  index_indents_on_district_id   (district_id)
 #  index_indents_on_extension_id  (extension_id)
 #  index_indents_on_region_id     (region_id)
-#  index_indents_on_stock_id      (stock_id)
+#  index_indents_on_user_id       (user_id)
 #
 # Foreign Keys
 #
 #  fk_rails_...  (district_id => districts.id)
 #  fk_rails_...  (extension_id => extensions.id)
 #  fk_rails_...  (region_id => regions.id)
-#  fk_rails_...  (stock_id => stocks.id)
+#  fk_rails_...  (user_id => users.id)
 #
 FactoryBot.define do
   factory :indent do
