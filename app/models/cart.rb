4517c2349526e259548f90360bc0f1aee214c286
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-01-11 17:21:09 +0600

    Cart features

---
+++ app/models/cart.rb
@@ -0,0 +1,20 @@
+# == Schema Information
+#
+# Table name: carts
+#
+#  id            :bigint           not null, primary key
+#  amount        :decimal(, )
+#  cartable_type :string           not null
+#  quantity      :decimal(, )
+#  created_at    :datetime         not null
+#  updated_at    :datetime         not null
+#  cartable_id   :bigint           not null
+#  session_id    :bigint
+#
+# Indexes
+#
+#  index_carts_on_cartable  (cartable_type,cartable_id)
+#
+class Cart < ApplicationRecord
+  include Relations::Cart
+end
