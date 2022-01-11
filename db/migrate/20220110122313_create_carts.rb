999efb37187ffe8cea19464dfad07ab76d43d5fa
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-01-11 17:21:09 +0600

    Cart features

---
+++ db/migrate/20220110122313_create_carts.rb
@@ -0,0 +1,12 @@
+class CreateCarts < ActiveRecord::Migration[6.1]
+  def change
+    create_table :carts do |t|
+      t.references :cartable, polymorphic: true, null: false
+      t.decimal :quantity
+      t.decimal :amount
+      t.bigint :session_id
+
+      t.timestamps
+    end
+  end
+end
