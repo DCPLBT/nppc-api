c9e947bb1685083d0e489d0c0224be78b822561d
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-01-13 10:14:33 +0600

    Update quantity after distribution

--- app/models/concerns/callbacks/distribution.rb
+++ app/models/concerns/callbacks/distribution.rb
@@ -6,13 +6,22 @@ module Callbacks
 
     included do
       before_create :assign_defaults
+      after_create :update_stock
     end
+
+    private
 
     def assign_defaults
       assign_attributes(
         reference_no: CodeGenerator.new.generate,
         state: :distributed
       )
+    end
+
+    def update_stock
+      line_items.each do |li|
+        li.stock&.update(quantity: (li.stock&.quantity || 0) - li.quantity)
+      end
     end
   end
 end
