d83e0c4f190c79a395d27e20e89f344e8b95d9ca
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-02-18 19:04:09 +0600

    Skip authentication for citizen

--- app/controllers/api/v1/citizens_controller.rb
+++ app/controllers/api/v1/citizens_controller.rb
@@ -3,6 +3,8 @@
 module Api
   module V1
     class CitizensController < ApplicationController
+      skip_before_action :authenticate_user!, only: :show
+
       def show
         show_citizen_form(citizen_form)
       end
