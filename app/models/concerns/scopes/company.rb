29a9303ed1a8597b478e307a9f09214fd3159673
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-03-01 18:40:33 +0600

    Order by id for masters

--- app/models/concerns/scopes/company.rb
+++ app/models/concerns/scopes/company.rb
@@ -5,6 +5,7 @@ module Scopes
     extend ActiveSupport::Concern
 
     included do
+      default_scope -> { order(:id) }
       scope :search, lambda { |query|
         where(
           "CONCAT_WS(' ', name) iLIKE ?", "%#{query&.squish}%"
