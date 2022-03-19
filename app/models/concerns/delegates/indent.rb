4c4007f6338caaa3d4de18ee4b57e3a61ff3a7ff
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-03-20 00:28:12 +0600

    Delegate region, district & extension name

--- app/models/concerns/delegates/indent.rb
+++ app/models/concerns/delegates/indent.rb
@@ -18,7 +18,10 @@ module Delegates
       delegate(:name, to: :extension, prefix: true, allow_nil: true)
       delegate(:name, to: :company, prefix: true, allow_nil: true)
       delegate(:name, to: :accepted_by, prefix: true, allow_nil: true)
-      delegate(:name, to: :user, prefix: true, allow_nil: true)
+      delegate(
+        :name, :region_name, :district_name, :extension_name, :company_name, :village_name,
+        to: :user, prefix: true, allow_nil: true
+      )
     end
   end
 end
