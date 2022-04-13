1f878267f4f4aaf058ac291195eec1ab910413cb
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-04-13 12:41:41 +0600

    Define mail from

--- app/mailers/concerns/display_name_assigner.rb
+++ app/mailers/concerns/display_name_assigner.rb
@@ -6,7 +6,7 @@ module DisplayNameAssigner
   private
 
   def default_sender_address
-    address = Mail::Address.new("no-reply@#{URI.parse(ENV['HOST_URL']).host}")
+    address = Mail::Address.new(ENV['MAIL_FROM'] || "no-reply@#{URI.parse(ENV['HOST_URL']).host}")
     address.display_name = ENV.fetch('MAIL_DISPLAY_NAME', Rails.application.class.module_parent.to_s)
     address
   end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -18,7 +18,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = "no-reply@#{URI.parse(ENV['HOST_URL']).host}"
+  config.mailer_sender = ENV['MAIL_FROM'] || "no-reply@#{URI.parse(ENV['HOST_URL']).host}"
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'UserMailer'
