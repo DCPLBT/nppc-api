147fc206d7148a2507ddf94d5c87937742982502
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-05-17 13:09:38 +0600

    Obsolete mailer changed

--- app/mailers/obsolete_mailer.rb
+++ app/mailers/obsolete_mailer.rb
@@ -2,7 +2,7 @@
 
 class ObsoleteMailer < ApplicationMailer
   def notify_obsolete_product(stock)
-    @stock = Stock.find(id)
+    @stock = stock
     @time = stock.obsolete_date
     mail(to: stock.group&.users&.pluck(:email))
   end
