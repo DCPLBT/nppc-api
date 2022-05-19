2d6abbde6628fd5fb8e3da8a9f8571bff0113292
Tashi Dendup <tashi.dendup@dcpl.bt>
2022-05-19 10:53:44 +0600

    Update quantity

--- app/models/concerns/callbacks/stock.rb
+++ app/models/concerns/callbacks/stock.rb
@@ -24,7 +24,7 @@ module Callbacks
     end
 
     def update_total_quantity_procured
-      update_columns(total_quantity_procured: (total_quantity_procured || 0) + quantity)
+      update_columns(total_quantity_procured: quantity)
     end
   end
 end
